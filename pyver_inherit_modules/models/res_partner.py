4b40232df489ff8546ffda8cd1a802f2eeeba567
Cris-aian Vergara <crisaianvergara@gmail.com>
2024-06-30 13:41:41 +0800

    [IMP] pyver_inherit_modules: GH:26 - Added loans status button to the contacts module.

--- pyver_inherit_modules/models/res_partner.py
+++ pyver_inherit_modules/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class ResPartner(models.Model):
@@ -6,4 +6,23 @@ class ResPartner(models.Model):
 
     barangay = fields.Char(string="Barangay")
     province = fields.Char(string="Province")
+    loan_ids = fields.One2many("loan.request", "partner_id", string="Loans")
+    loan_count = fields.Integer("Loans", compute="_compute_loan_count")
+
+    @api.depends("loan_ids")
+    def _compute_loan_count(self):
+        """Count requested loans."""
+        for partner in self:
+            partner.loan_count = len(partner.loan_ids)
     
+    def action_view_partner_loans(self):
+        """Redirect to the loan request records of the current partner."""
+        self.ensure_one()
+        return {
+            'name': _('Loans'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+            'domain': [('partner_id', '=', self.id)],
+            'context': {'default_partner_id': self.id},
+            'res_model': 'loan.request',
+        }
